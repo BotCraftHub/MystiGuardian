d619b3d70c235c8e62775408762012ced0ec6bde
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2023-11-14 22:43:30 +0000

    added messagte that would be sent to user if they banned/warned etc

--- src/main/java/io/github/yusufsdiscordbot/mystiguardian/event/listener/ModerationActionTriggerEventListener.java
+++ src/main/java/io/github/yusufsdiscordbot/mystiguardian/event/listener/ModerationActionTriggerEventListener.java
@@ -11,30 +11,6 @@ import org.javacord.api.entity.user.User;
 import org.jetbrains.annotations.NotNull;
 
 public class ModerationActionTriggerEventListener implements ModerationActionTriggerEventHandler {
-    @NotNull
-    private static EmbedBuilder getEmbedBuilder(ModerationActionTriggerEvent event, User user, User admin, long moderationActionId) {
-        val embedBuilder = new EmbedBuilder();
-        embedBuilder.setTitle(STR."\{user.getDiscriminatedName()} was \{event.getModerationTypes().getName().toLowerCase()}ed");
-        embedBuilder.addField("Reason", event.getReason());
-        embedBuilder.addField("Admin", admin.getDiscriminatedName());
-        embedBuilder.setFooter(STR."User id: \{user.getIdAsString()} | \{event.getModerationTypes().getName().toLowerCase()} id: \{moderationActionId}");
-        embedBuilder.setTimestampToNow();
-        embedBuilder.setColor(MystiGuardianUtils.getBotColor());
-        embedBuilder.setAuthor(event.getApi().getYourself());
-        return embedBuilder;
-    }
-
-    private static EmbedBuilder getMessageDeletedEmbed(ModerationActionTriggerEvent event, User admin, Integer amountOfMessagesDeleted) {
-        val embedBuilder = new EmbedBuilder();
-        embedBuilder.setTitle(STR."\{amountOfMessagesDeleted} messages were deleted");
-        embedBuilder.addField("Admin", admin.getDiscriminatedName());
-        embedBuilder.setFooter(STR."Admin id: \{admin.getIdAsString()}");
-        embedBuilder.setTimestampToNow();
-        embedBuilder.setColor(MystiGuardianUtils.getBotColor());
-        embedBuilder.setAuthor(event.getApi().getYourself());
-        return embedBuilder;
-    }
-
     @Override
     public void onModerationActionTriggerEvent(ModerationActionTriggerEvent event) {
         val systemChannel = event.getApi().getServerById(event.getServerId())
@@ -56,13 +32,66 @@ public class ModerationActionTriggerEventListener implements ModerationActionTri
             val user = event.getApi().getUserById(event.getUserId())
                     .join();
 
-            final var embedBuilder = getEmbedBuilder(event, user, admin, event.getModerationActionId());
-            systemChannel.sendMessage(embedBuilder);
+            val embedBuilder = getEmbedBuilder(event, user, admin, event.getModerationActionId());
+            systemChannel.sendMessage(embedBuilder)
+                    .thenAccept(message -> {
+                        user.openPrivateChannel()
+                                .thenAccept(privateChannel -> {
+                                    val userEmbedBuilder = getUserEmbedBuilder(event, user, admin, event.getModerationActionId());
+                                    privateChannel.sendMessage(userEmbedBuilder);
+                                });
+                    });
         }
 
         if (event.getModerationTypes() == MystiGuardianUtils.ModerationTypes.DELETE_MESSAGES) {
             val embedBuilder = getMessageDeletedEmbed(event, admin, event.getAmountOfMessagesDeleted());
             systemChannel.sendMessage(embedBuilder);
         }
+    }
+
+
+    @NotNull
+    private static EmbedBuilder getEmbedBuilder(@NotNull ModerationActionTriggerEvent event, @NotNull User user, User admin, long moderationActionId) {
+        val embedBuilder = new EmbedBuilder();
+
+        embedBuilder.setTitle(STR."\{user.getDiscriminatedName()} was \{event.getModerationTypes().getName().toLowerCase()}ed");
+        embedBuilder.setFooter(STR."User id: \{user.getIdAsString()} | \{event.getModerationTypes().getName().toLowerCase()} id: \{moderationActionId}");
+        embedBuilder.setTimestampToNow();
+        embedBuilder.setColor(MystiGuardianUtils.getBotColor());
+        embedBuilder.setAuthor(event.getApi().getYourself());
+        similarFields(embedBuilder, event, admin);
+
+        return embedBuilder;
+    }
+
+    @NotNull
+    private static EmbedBuilder getMessageDeletedEmbed(@NotNull ModerationActionTriggerEvent event, @NotNull User admin, Integer amountOfMessagesDeleted) {
+        val embedBuilder = new EmbedBuilder();
+
+        embedBuilder.setTitle(STR."\{amountOfMessagesDeleted} messages were deleted");
+        embedBuilder.addField("Admin", admin.getDiscriminatedName());
+        embedBuilder.setFooter(STR."Admin id: \{admin.getIdAsString()}");
+        embedBuilder.setTimestampToNow();
+        embedBuilder.setColor(MystiGuardianUtils.getBotColor());
+        embedBuilder.setAuthor(event.getApi().getYourself());
+        return embedBuilder;
+    }
+
+    @NotNull
+    private static EmbedBuilder getUserEmbedBuilder(@NotNull ModerationActionTriggerEvent event, User user, User admin, long moderationActionId) {
+        val embedBuilder = new EmbedBuilder();
+
+        embedBuilder.setTitle(STR."You were \{event.getModerationTypes().getName().toLowerCase()}ed");
+        embedBuilder.setFooter(STR."\{event.getModerationTypes().getName().toLowerCase()} id: \{moderationActionId}");
+        embedBuilder.setTimestampToNow();
+        embedBuilder.setColor(MystiGuardianUtils.getBotColor());
+        embedBuilder.setAuthor(event.getApi().getYourself());
+        similarFields(embedBuilder, event, admin);
+
+        return embedBuilder;
+    }
+
+    private static void similarFields(@NotNull EmbedBuilder embedBuilder, @NotNull ModerationActionTriggerEvent event, @NotNull User admin) {
+        embedBuilder.addField("Reason", event.getReason());
     }
 }
